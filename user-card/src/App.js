bd7b4930396859eba9a9d4456b1ad5541a2d9c49
Bradley Dion <bradleydion@MacBook-Pro.local>
2020-12-06 12:40:50 -0500

    adding some more structure in my constructor and working in the correct file in terminal

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -3,6 +3,14 @@ import React from "react"
 import axios from "axios"
 
 class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      users:[],
+      userName:""
+    }
+  }
+  handleChanges
 
 }
 
